7c3ccb4a5603fe6762525e8987a206621876d6eb
parmeshwar <parmesh1042@gmail.com>
2018-11-25 11:00:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs')
+
+hbs.registerHelper('getCurrentYear', () => {
+
+    return new Date().getFullYear();
+});
+hbs.registerHelper('changeFont', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    var log = `${new Date().toString()} path ${req.path} method ${req.method}`;
+    fs.appendFile('server.log',log+'\n', (error) => {
+        if(error){
+            console.log(error);
+        }
+        next();
+    });
+});
+app.use((req, res, next) => {
+    res.render('maintanence.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        welcome : 'Hi this is Express Appliaciton'
+    });
+});
+
+app.get('/about', (req, res) => {
+        res.render('about.hbs',{
+            pageTitle : 'About Page'
+        });
+});
+app.listen(3000, () => {
+    console.log('server is up and running on port 3000');
+});
